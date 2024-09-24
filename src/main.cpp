aa92eb7f11ac46c3c67442b3079c091d50f4cb16
Gaute <mrboobai@proton.me>
2024-09-24 08:18:31 +0200

    newest

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-const int butPin = 9;
+const int butPin = 8;
 
-const int pin = 8;
+const int pin = 7;
 const int numPixels = 5;
 const int delayVal = 100;
 
@@ -16,7 +16,7 @@ Adafruit_NeoPixel pixels(numPixels, pin, NEO_GRB + NEO_KHZ800);
 void setup()
 {
   Serial.begin(9600);
-  pinMode(butPin, OUTPUT);
+  pinMode(butPin, INPUT);
   pixels.begin(); // Initialize the NeoPixel strip
 }
 
@@ -29,13 +29,20 @@ float normalizeToRange(int value, int old_min, int old_max, int new_min, int new
 
 void loop()
 {
-  int butOut = digitalRead(butPin);
-  Serial.print(butOut);
+  int buttonState = digitalRead(butPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    Serial.println(buttonState);
+    Serial.print("pressed");
+  } else {
+    Serial.println(buttonState);
+  }
 
   int potValue = analogRead(potentiometerPin);
   int NormalPotValue = normalizeToRange(potValue, 0, 1023, 0, 5);
-  Serial.print("Normalized Potentiometer Value: ");
-  Serial.println(NormalPotValue);
+  // Serial.print("Normalized Potentiometer Value: ");
+  // Serial.println(NormalPotValue);
   
   pixels.clear(); // Clear the pixel buffer
 
@@ -47,21 +54,31 @@ void loop()
     break;
 
   case 2:
+    pixels.setPixelColor(0, pixels.Color(0, 255, 0));
     pixels.setPixelColor(1, pixels.Color(0, 255, 0));
     pixels.show();
     break;
 
   case 3:
+    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+    pixels.setPixelColor(1, pixels.Color(0, 0, 255));
     pixels.setPixelColor(2, pixels.Color(0, 0, 255));
     pixels.show();
     break;
 
   case 4:
+    pixels.setPixelColor(0, pixels.Color(255, 255, 0));
+    pixels.setPixelColor(1, pixels.Color(255, 255, 0));
+    pixels.setPixelColor(2, pixels.Color(255, 255, 0));
     pixels.setPixelColor(3, pixels.Color(255, 255, 0));
     pixels.show();
     break;
 
   case 5:
+    pixels.setPixelColor(0, pixels.Color(0, 255, 255));
+    pixels.setPixelColor(1, pixels.Color(0, 255, 255));
+    pixels.setPixelColor(2, pixels.Color(0, 255, 255));
+    pixels.setPixelColor(3, pixels.Color(0, 255, 255));
     pixels.setPixelColor(4, pixels.Color(0, 255, 255));
     pixels.show();
     break;
